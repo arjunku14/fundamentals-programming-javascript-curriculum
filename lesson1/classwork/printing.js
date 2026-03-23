e911d708ac7505395f5ebb92b2a1330ada31a077
arjunku14 <arjun.gadamsetty6@gmail.com>
2026-03-23 00:58:10 +0000

    Fineshed Lesson 1

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello, world");
+console.log("My name is Arjun");
+console.log('My favorite food is dosa');
+
+// Error: quatation marks mismatched
+// console.log ('This doesent work");
