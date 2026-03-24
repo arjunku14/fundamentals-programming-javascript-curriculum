e94f091d12bb3609e310d427716b24423f7d2037
arjunku14 <arjun.gadamsetty6@gmail.com>
2026-03-24 00:32:56 +0000

    Fineshed Lesson 1 homework

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,24 +1,32 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
+let x = 5;
+console.log(x);
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
+let y = 9;
+console.log(y);
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
+let z = "strawberry";
+console.log(z);
 
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
-
-
-
+let city = "Bothell"
+let country = "USA"
+console.log (city);
+console.log(country);
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+let petname = 'Goku'
+let age = 6
+let type = 'snake'
+console.log (petname + age + type);
